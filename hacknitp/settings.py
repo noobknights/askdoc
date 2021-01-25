9ffa628c6ae9706a71e2bf4fca039d1762077abb
devblin <deepanshu.dhruw@outlook.com>
2021-01-25 22:48:12 +0530

    updated UI

--- hacknitp/settings.py
+++ hacknitp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('DEV_HOST'), os.getenv('PRODUCTION_HOST'), '7b0d4428c7df.ngrok.io']
+ALLOWED_HOSTS = [os.getenv('DEV_HOST'), os.getenv('PRODUCTION_HOST')]
 
 
 # Application definition
